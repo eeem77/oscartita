d1fc3d5b105222ddf7e8738da5df970df255c5f0
eeem77 <escobarmernestoe@gmail.com>
2017-09-23 10:00:52 -0400

    arreglos varios
    arreglos del sistema en general

---
+++ bandeja.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<!--suppress ALL -->
+<html>
+<head>
+    <title>Electroauto Oscartita</title>
+    <link rel="Shortcut Icon" type="image/x-icon" href="img/iconos/mecanica.ico"/>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/estilo.css">
+    <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+    <script type="text/javascript" src="js/jquery.expander.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <meta charset="utf-8">
+</head>
+<body>
+<div class="container-fluid">
+    <div class="row center-block">
+        <table id="img-tabla" class="table-responsive">
+            <tr>
+                <td><img class="img-responsive" src="img/img-marcas-carros/CHERY.png" alt=""></td>
+                <td><img class="img-responsive" src="img/img-marcas-carros/CHEVROLET.png" alt=""></td>
+                <td><img class="img-responsive" src="img/img-marcas-carros/CHRYSLER.png" alt=""></td>
+                <td><img class="img-responsive" src="img/img-marcas-carros/DODGE.png" alt=""></td>
+                <td><img class="img-responsive" src="img/img-marcas-carros/FIAT.png" alt=""></td>
+                <td><img class="img-responsive" src="img/img-marcas-carros/FORD.png" alt=""></td>
+                <td><img class="img-responsive" src="img/img-marcas-carros/HONDA.png" alt=""></td>
+                <td><img class="img-responsive" src="img/img-marcas-carros/HYUNDAI.png" alt=""></td>
+                <td><img class="img-responsive" src="img/img-marcas-carros/JEEP.png" alt=""></td>
+                <td><img class="img-responsive" src="img/img-marcas-carros/MAZDA.png" alt=""></td>
+                <td><img class="img-responsive" src="img/img-marcas-carros/MITSUBISHI.png" alt=""></td>
+                <td><img class="img-responsive" src="img/img-marcas-carros/PEUGEOT.png" alt=""></td>
+                <td><img class="img-responsive" src="img/img-marcas-carros/TOYOTA.png" alt=""></td>
+                <td><img class="img-responsive" src="img/img-marcas-carros/VOLSKWAGEN.png" alt=""></td>
+            </tr>
+        </table>
+    </div>
+    <div class="row">
+        <ul class="nav nav-tabs">
+            <li role="presentation"><a href="inicio.html">Inicio</a></li>
+            <li role="presentation"><a href="servicios.html">Servicios</a></li>
+            <li role="presentation" class="active"><a href="#">Admin</a></li>
+        </ul>
+    </div>
+</div>
+</body>
+</html>
