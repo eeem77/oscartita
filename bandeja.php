c7fd082cc4635e5747172299ff5342e019b0e520
eeem77 <escobarmernestoe@gmail.com>
2017-09-23 15:04:57 -0400

    creacion del panel de reporte de solicitudes
    se crea el panel de reporte de solicitudes, ademas se correigen error en identacion del codigo

--- bandeja.php
+++ bandeja.php
@@ -1,45 +1,90 @@
 <!DOCTYPE html>
 <!--suppress ALL -->
 <html>
-<head>
-    <title>Electroauto Oscartita</title>
-    <link rel="Shortcut Icon" type="image/x-icon" href="img/iconos/mecanica.ico"/>
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="css/estilo.css">
-    <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
-    <script type="text/javascript" src="js/jquery.expander.js"></script>
-    <script type="text/javascript" src="js/bootstrap.min.js"></script>
-    <meta charset="utf-8">
-</head>
-<body>
-<div class="container-fluid">
-    <div class="row center-block">
-        <table id="img-tabla" class="table-responsive">
-            <tr>
-                <td><img class="img-responsive" src="img/img-marcas-carros/CHERY.png" alt=""></td>
-                <td><img class="img-responsive" src="img/img-marcas-carros/CHEVROLET.png" alt=""></td>
-                <td><img class="img-responsive" src="img/img-marcas-carros/CHRYSLER.png" alt=""></td>
-                <td><img class="img-responsive" src="img/img-marcas-carros/DODGE.png" alt=""></td>
-                <td><img class="img-responsive" src="img/img-marcas-carros/FIAT.png" alt=""></td>
-                <td><img class="img-responsive" src="img/img-marcas-carros/FORD.png" alt=""></td>
-                <td><img class="img-responsive" src="img/img-marcas-carros/HONDA.png" alt=""></td>
-                <td><img class="img-responsive" src="img/img-marcas-carros/HYUNDAI.png" alt=""></td>
-                <td><img class="img-responsive" src="img/img-marcas-carros/JEEP.png" alt=""></td>
-                <td><img class="img-responsive" src="img/img-marcas-carros/MAZDA.png" alt=""></td>
-                <td><img class="img-responsive" src="img/img-marcas-carros/MITSUBISHI.png" alt=""></td>
-                <td><img class="img-responsive" src="img/img-marcas-carros/PEUGEOT.png" alt=""></td>
-                <td><img class="img-responsive" src="img/img-marcas-carros/TOYOTA.png" alt=""></td>
-                <td><img class="img-responsive" src="img/img-marcas-carros/VOLSKWAGEN.png" alt=""></td>
-            </tr>
-        </table>
-    </div>
-    <div class="row">
-        <ul class="nav nav-tabs">
-            <li role="presentation"><a href="inicio.html">Inicio</a></li>
-            <li role="presentation"><a href="servicios.html">Servicios</a></li>
-            <li role="presentation" class="active"><a href="#">Admin</a></li>
-        </ul>
-    </div>
-</div>
-</body>
+    <head>
+        <title>Electroauto Oscartita</title>
+        <link rel="Shortcut Icon" type="image/x-icon" href="img/iconos/mecanica.ico"/>
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="css/estilo.css">
+        <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+        <script type="text/javascript" src="js/jquery.expander.js"></script>
+        <script type="text/javascript" src="js/bootstrap.min.js"></script>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row center-block">
+                <table id="img-tabla" class="table-responsive">
+                    <tr>
+                        <td><img class="img-responsive" src="img/img-marcas-carros/CHERY.png" alt=""></td>
+                        <td><img class="img-responsive" src="img/img-marcas-carros/CHEVROLET.png" alt=""></td>
+                        <td><img class="img-responsive" src="img/img-marcas-carros/CHRYSLER.png" alt=""></td>
+                        <td><img class="img-responsive" src="img/img-marcas-carros/DODGE.png" alt=""></td>
+                        <td><img class="img-responsive" src="img/img-marcas-carros/FIAT.png" alt=""></td>
+                        <td><img class="img-responsive" src="img/img-marcas-carros/FORD.png" alt=""></td>
+                        <td><img class="img-responsive" src="img/img-marcas-carros/HONDA.png" alt=""></td>
+                        <td><img class="img-responsive" src="img/img-marcas-carros/HYUNDAI.png" alt=""></td>
+                        <td><img class="img-responsive" src="img/img-marcas-carros/JEEP.png" alt=""></td>
+                        <td><img class="img-responsive" src="img/img-marcas-carros/MAZDA.png" alt=""></td>
+                        <td><img class="img-responsive" src="img/img-marcas-carros/MITSUBISHI.png" alt=""></td>
+                        <td><img class="img-responsive" src="img/img-marcas-carros/PEUGEOT.png" alt=""></td>
+                        <td><img class="img-responsive" src="img/img-marcas-carros/TOYOTA.png" alt=""></td>
+                        <td><img class="img-responsive" src="img/img-marcas-carros/VOLSKWAGEN.png" alt=""></td>
+                    </tr>
+                </table>
+            </div>
+            <div class="row">
+                <ul class="nav nav-tabs">
+                    <li role="presentation"><a href="inicio.html">Inicio</a></li>
+                    <li role="presentation"><a href="servicios.html">Servicios</a></li>
+                    <li role="presentation" class="active"><a href="#">Admin</a></li>
+                </ul>
+            </div>
+            <div class="row row-servicios">
+
+                <?php
+                $servername = "localhost";
+                $username = "root";
+                $password = "";
+                $dbname = "oscartita";
+
+                // Creamos la conexion con la base de datos
+                $conn = new mysqli($servername, $username, $password, $dbname);
+
+                // Chequeo de la conexion
+                if ($conn->connect_error) {
+                    die("Connection failed: " . $conn->connect_error);
+                }
+
+                $sql = "SELECT id, nombres, apellidos, contactos, correos, marca_modelo, observaciones, servicios, fecha FROM clientes_problemas";
+                $result = $conn->query($sql);
+
+                if ($result->num_rows > 0) {
+                    while ($row = $result->fetch_assoc()) {
+                        echo "
+                        <div class=\"col-xs-12 col-ms-12 col-md-12 col-lg-12\">
+                            <div class=\"panel panel-default panel-primary\">
+                                <div class=\"panel-heading\">Solicitud de servicio</div>
+                                    <!-- Default panel contents -->
+                                <div class=\"panel-body\">
+                                    <p class=\"text-center\">
+                                        <b>Servicio</b><br>" . $row["id"] . "<br>
+                                        <b>Nombre</b><br>" . $row["nombres"] . "<br>
+                                        <b>Apellido</b><br>" . $row["apellidos"] . "<br>
+                                        <b>Numero de contacto</b><br>" . $row["contactos"] . "<br>
+                                        <b>Correo</b><br>" . $row["correos"] . "<br>
+                                        <b>Caracteristicas del vehiculo</b><br>" . $row["marca_modelo"] . "<br>
+                                        <b>Observaciones</b><br>" . $row["observaciones"] . "<br>
+                                        <b>Servicios solicitados</b><br>" . $row["servicios"] . "<br>
+                                        <b>Fecha</b><br>" . $row["fecha"] . "<br>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>";
+                    }
+                }
+                ?>
+            </div>
+        </div>
+    </body>
 </html>
--- validacion.php
+++ validacion.php
@@ -1,134 +1,134 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Electroauto Oscartita</title>
-    <link rel="Shortcut Icon" type="image/x-icon" href="img/iconos/mecanica.ico"/>
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="css/estilo.css">
-    <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
-    <script type="text/javascript" src="js/jquery.expander.js"></script>
-    <script type="text/javascript" src="js/bootstrap.min.js"></script>
-    <meta charset="utf-8">
-</head>
-<body>
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "oscartita";
+    <head>
+        <title>Electroauto Oscartita</title>
+        <link rel="Shortcut Icon" type="image/x-icon" href="img/iconos/mecanica.ico"/>
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="css/estilo.css">
+        <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+        <script type="text/javascript" src="js/jquery.expander.js"></script>
+        <script type="text/javascript" src="js/bootstrap.min.js"></script>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <?php
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "oscartita";
 
-// Creacion de la conexion con la base de datos
-$conn = new mysqli($servername, $username, $password, $dbname);
+        // Creacion de la conexion con la base de datos
+        $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Chequeo de la conexion
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+        // Chequeo de la conexion
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
 
-// Mensaje de conexion
-//echo "Conexion con la base de datos de forma correcta";
-//echo "<br>";
+        // Mensaje de conexion
+        //echo "Conexion con la base de datos de forma correcta";
+        //echo "<br>";
 
-$name = $apellido = $celular = $email = $vehiculo = "";
+        $name = $apellido = $celular = $email = $vehiculo = "";
 
-function test_input($data){
-    $data = trim($data);
-    $data = stripcslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
-}
+        function test_input($data){
+            $data = trim($data);
+            $data = stripcslashes($data);
+            $data = htmlspecialchars($data);
+            return $data;
+        }
 
-$nombre = test_input($_POST["nombre"]);
-$apellido = test_input($_POST["apellido"]);
-$celular = test_input($_POST["celular"]);
-$email = test_input($_POST["email"]);
-$vehiculo = test_input($_POST["vehiculo"]);
-$obs = test_input($_POST["obs"]);
-$fecha = date("d") ."/". date("m") ."/". date("Y");
-$s_a = "";
-$s_e = "";
-$s_i = "";
-$s_m = "";
+        $nombre = test_input($_POST["nombre"]);
+        $apellido = test_input($_POST["apellido"]);
+        $celular = test_input($_POST["celular"]);
+        $email = test_input($_POST["email"]);
+        $vehiculo = test_input($_POST["vehiculo"]);
+        $obs = test_input($_POST["obs"]);
+        $fecha = date("d") ."/". date("m") ."/". date("Y");
+        $s_a = "";
+        $s_e = "";
+        $s_i = "";
+        $s_m = "";
 
-if (isset($_POST['alternador']) !="") {
-    $s_a = " Alternador";
-}
-if (isset($_POST['escaner']) !="") {
-    $s_e = " Escaner";
-}
-if (isset($_POST['inyector']) !="") {
-    $s_i = " Inyectores";
-}
-if (isset($_POST['mecanica']) !="") {
-    $s_m = " Mecanica";
-}
+        if (isset($_POST['alternador']) !="") {
+            $s_a = " Alternador";
+        }
+        if (isset($_POST['escaner']) !="") {
+            $s_e = " Escaner";
+        }
+        if (isset($_POST['inyector']) !="") {
+            $s_i = " Inyectores";
+        }
+        if (isset($_POST['mecanica']) !="") {
+            $s_m = " Mecanica";
+        }
 
-$servicio = $s_a . $s_e . $s_i . $s_m;
+        $servicio = $s_a . $s_e . $s_i . $s_m;
 
-$sql = "INSERT INTO clientes_problemas (nombres, apellidos, contactos, correos, marca_modelo, observaciones, servicios, fecha) VALUES ('".$nombre."','".$apellido."','".$celular."','".$email."','".$vehiculo."','".$obs."','".$servicio."','".$fecha."')";
+        $sql = "INSERT INTO clientes_problemas (nombres, apellidos, contactos, correos, marca_modelo, observaciones, servicios, fecha) VALUES ('".$nombre."','".$apellido."','".$celular."','".$email."','".$vehiculo."','".$obs."','".$servicio."','".$fecha."')";
 
-if ($conn->query($sql) === TRUE) {
-    //echo "se insertaron los datos en la tabla";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->connect_error;
-}
+        if ($conn->query($sql) === TRUE) {
+            //echo "se insertaron los datos en la tabla";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->connect_error;
+        }
 
-$conn->close();
+        $conn->close();
 
-/**
- * Created by PhpStorm
- * User: eeem77
- * Date: 23/5/2017
- * Time: 11:06 AM
- */
-?>
-<div class="container-fluid">
-    <div class="jumbotron con">
-        <h1>Electro Auto Oscartita C.A.</h1>
-        <p class="text-muted">J-40439176-2</p>
-        <p><?php echo "Sr(a) " . $nombre ?>. Gracias por confiar en nosotros. Pronto nos comunicaremos con usted.</p>
-        <p><a class="btn btn-primary btn-lg btn-index" href="inicio.html" role="button">Continuar</a></p>
-    </div>
-</div>
-<?php
-$destinatario = "montimarq@gmail.com";
-$asunto = "Electro Auto Oscartita C.A.";
-$cuerpo = ' 
-<html> 
-<head> 
-   <title>Electro Auto Oscartita C.A.</title> 
-</head> 
-<body> 
-<h1>Nuestros clientes son primero!</h1> 
-<p> 
-<b>Bienvenidos al servicio de correos de Electro Auto Oscartita C.A.</b>. Ofrecemos el mejor servicio para su vehiculo. 
-</p> 
-</body> 
-</html> 
-';
+        /**
+         * Created by PhpStorm
+         * User: eeem77
+         * Date: 23/5/2017
+         * Time: 11:06 AM
+         */
+        ?>
+        <div class="container-fluid">
+            <div class="jumbotron con">
+                <h1>Electro Auto Oscartita C.A.</h1>
+                <p class="text-muted">J-40439176-2</p>
+                <p><?php echo "Sr(a) " . $nombre ?>. Gracias por confiar en nosotros. Pronto nos comunicaremos con usted.</p>
+                <p><a class="btn btn-primary btn-lg btn-index" href="inicio.html" role="button">Continuar</a></p>
+            </div>
+        </div>
+        <?php
+        $destinatario = "marlo.montilla@gmail.com";
+        $asunto = "Electro Auto Oscartita C.A.";
+        $cuerpo = ' 
+            <html> 
+                <head> 
+                   <title>Electro Auto Oscartita C.A.</title> 
+                </head> 
+                <body> 
+                    <h1>Nuestros clientes son primero!</h1> 
+                    <p> 
+                        <b>Bienvenidos al servicio de correos de Electro Auto Oscartita C.A.</b>. Ofrecemos el mejor servicio para su vehiculo. 
+                    </p> 
+                </body> 
+            </html> 
+        ';
 
-//para el envío en formato HTML
-$headers = "MIME-Version: 1.0\r\n";
-$headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
+        //para el envío en formato HTML
+        $headers = "MIME-Version: 1.0\r\n";
+        $headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
 
-//dirección del remitente
-$headers .= "From: Electro Auto Oscartita C.A. <electroautoscartita@gmail.com>\r\n";
+        //dirección del remitente
+        $headers .= "From: Electro Auto Oscartita C.A. <electroautoscartita@gmail.com>\r\n";
 
-//dirección de respuesta, si queremos que sea distinta que la del remitente
-//$headers .= "Reply-To: mariano@desarrolloweb.com\r\n";
+        //dirección de respuesta, si queremos que sea distinta que la del remitente
+        //$headers .= "Reply-To: mariano@desarrolloweb.com\r\n";
 
-//ruta del mensaje desde origen a destino
-//$headers .= "Return-path: holahola@desarrolloweb.com\r\n";
+        //ruta del mensaje desde origen a destino
+        //$headers .= "Return-path: holahola@desarrolloweb.com\r\n";
 
-//direcciones que recibián copia
-//$headers .= "Cc: maria@desarrolloweb.com\r\n";
+        //direcciones que recibián copia
+        //$headers .= "Cc: maria@desarrolloweb.com\r\n";
 
-//direcciones que recibirán copia oculta
-//$headers .= "Bcc: pepe@pepe.com,juan@juan.com\r\n";
+        //direcciones que recibirán copia oculta
+        //$headers .= "Bcc: pepe@pepe.com,juan@juan.com\r\n";
 
-mail($destinatario,$asunto,$cuerpo,$headers)
-?>
-</body>
+        mail($destinatario,$asunto,$cuerpo,$headers)
+        ?>
+    </body>
 </html>
 
 
